12e605a14466aa4e1c49dac20c107fbf24b16b2a
Jbuenoss <joaobueno121212@gmail.com>
2024-06-05 16:07:16 -0300

    Carousel finalized

--- src/components/CarouselComponent.jsx
+++ src/components/CarouselComponent.jsx
@@ -1,7 +1,7 @@
 import Carousel from 'react-bootstrap/Carousel';
-import logo from '../assets/Logo.jpg'; //.. is realtive way
+import Button from 'react-bootstrap/Button';
 
-import img1 from '../assets/africa-do-sul.jpg';
+import img1 from '../assets/africa-do-sul.jpg'; //.. is realtive way
 import img2 from '../assets/greece.jpg';
 import img3 from '../assets/paris.jpg';
 
@@ -11,32 +11,33 @@ import './style.css'
 function CarouselComponent(){
 
     return (
-        <Carousel>
+        <Carousel className='mt-3 ms-5 me-5 shadow'>
 
           <Carousel.Item className="c-item">
             <img src={img1} className="c-img"/>
-            <Carousel.Caption>
-              <h2 className="mb-5">Discover the world with us</h2>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <Carousel.Caption className='top-0'>
+              <h2 className="mb-3 fs-1">Discover the world with us</h2>
+              <Button variant="info" className="mb-5 px-4 py-2">Book a tour</Button>
+              <h3 className="display-3 fw-bold mt-5 text-capitalize">unforgettable journeys</h3>
+            </Carousel.Caption>
+            <Carousel.Caption >
+              <p>Discover the Splendor of South Africa</p>
             </Carousel.Caption>
           </Carousel.Item>
 
           <Carousel.Item className="c-item">
           <img src={img2} className="c-img"/>
             <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h3 className="display-3 fw-bold mt-5 text-capitalize">The season has arrived</h3>
+              <p>Escape to the Idyllic Shores of Greece</p>
             </Carousel.Caption>
           </Carousel.Item>
 
           <Carousel.Item className="c-item">
             <img src={img3} className="c-img"/>
             <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
+              <h3 className="display-3 fw-bold mt-5 text-capitalize">enchanting atmosphere</h3>
+              <p>Embrace the Romance of Paris</p>
             </Carousel.Caption>
           </Carousel.Item>
 
